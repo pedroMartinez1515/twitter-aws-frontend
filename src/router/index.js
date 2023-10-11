d10b6aa710c561bff162d7e58092634ff5a833a8
pedro martinez <mulinhazikoli@gmail.com>
2023-10-11 19:22:25 -0300

    setup router guard for authenticated pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,40 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Root from '../views/Root.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Root from "../views/Root.vue";
+import AuthMiddleware from "./auth.guard";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Root',
-    component: Root
+    path: "/",
+    name: "Root",
+    component: Root,
   },
   {
-    path: '/login',
-    name: 'LogIn',
+    path: "/login",
+    name: "LogIn",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "login" */ '../views/LogIn.vue')
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/LogIn.vue"),
   },
   {
-    path: '/home',
-    name: 'Home',
+    path: "/home",
+    name: "Home",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
-    meta: {protected: true}
-  }
-]
+    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue"),
+    meta: { protected: true },
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach(AuthMiddleware)
+
+export default router;
--- src/views/Root.vue
+++ src/views/Root.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <h1>Root</h1>
+    <amplify-authenticator usernameAlias="email">
+        <amplify-sign-out/>
+    </amplify-authenticator>
   </div>
 </template>
 
 <script>
-
 export default({
   name: 'Root'
 })
