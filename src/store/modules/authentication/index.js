5f707e3ae4e0266fa6ada5d9955e0854832491e8
pedro martinez <mulinhazikoli@gmail.com>
2023-10-16 15:59:38 -0300

    Vuex: add authentication module

--- src/store/modules/authentication/index.js
+++ src/store/modules/authentication/index.js
@@ -1,3 +1,6 @@
+import mutations from './mutations'
+import actions from './actions'
+import getters from './getters'
 const state = () => ({
     loggedIn: false,
     user: undefined
@@ -5,5 +8,8 @@ const state = () => ({
 
 export default {
     namespaced: true,
+    actions,
+    mutations,
+    getters,
     state,
 }
