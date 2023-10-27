fb5b030c89c826b8d99d1688926ff7272d71b656
pedro martinez <mulinhazikoli@gmail.com>
2023-10-27 17:21:00 -0300

    landing page: add signup Vuex module and migrate local signup logic to Vuex

--- src/views/Root.vue
+++ src/views/Root.vue
@@ -10,9 +10,7 @@
           </div>
           <div class="flex items-center mt-10">
             <i class="fas fa-user-friends text-white text-2xl mr-4"></i>
-            <p class="text-white font-bold text-xl">
-              Hear what people are talking about.
-            </p>
+            <p class="text-white font-bold text-xl">Hear what people are talking about.</p>
           </div>
           <div class="flex items-center mt-10">
             <i class="fas fa-comment text-white text-2xl mr-5"></i>
@@ -26,61 +24,41 @@
     <div class="flex w-1/2 h-full">
       <div class="flex flex-col w-1/2 font-bold">
         <i class="fab fa-twitter text-blue text-4xl mb-5"></i>
-        <p class="text-3xl mb-12">
-          See what's happening in the world right now
-        </p>
+        <p class="text-3xl mb-12">See what's happening in the world right now</p>
         <p>Join Twitter today.</p>
-        <button
-          @click.prevent="setSignupStep('step1')"
-          class="rounded-full bg-blue font-bold text-lg text-white mt-4 p-3 hover:bg-darkblue"
-        >
-          Sign up
-        </button>
-        <button
-          @click.prevent="showSignInPage"
-          class="rounded-full border border-blue bg-white font-bold text-lg text-blue mt-4 p-3 hover:bg-lightblue"
-        >
-          Log in
-        </button>
+        <button @click.prevent="setSignupStep('step1')" class="rounded-full bg-blue font-bold text-lg text-white mt-4 p-3 hover:bg-darkblue">Sign up</button>
+        <button @click.prevent="showSignInPage" class="rounded-full border border-blue bg-white font-bold text-lg text-blue mt-4 p-3 hover:bg-lightblue">Log in</button>
       </div>
     </div>
 
     <!-- sign up modal -->
-    <div
-      v-if="step != ''"
-      class="fixed w-full h-full top-0 left-0 flex items-center justify-center"
-    >
-      <div
-        class="absolute w-full h-full bg-gray-900 opacity-50"
-        @click.prevent="setSignupStep('')"
-      ></div>
+    <div v-if="showModal != ''" class="fixed w-full h-full top-0 left-0 flex items-center justify-center">
+      <div class="absolute w-full h-full bg-gray-900 opacity-50" @click.prevent="setSignupStep('')"></div>
 
-      <div
-        class="modal-main bg-white w-11/12 mx-auto rounded-lg z-50 overflow-y-auto max-h-full"
-      >
-        Modal
-      </div>
+      <div class="modal-main bg-white w-11/12 mx-auto rounded-lg z-50 overflow-y-auto max-h-full">Modal</div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex';
+
 export default {
-  name: "Root",
-  data (){
-    return {
-      step: ''
-    }
+  name: 'Root',
+  computed: {
+    ...mapGetters('signup', [
+      'showModal'
+    ]),
   },
   methods: {
     showSignInPage() {
-      this.$router.push("LogIn");
+      this.$router.push('LogIn')
     },
-    setSignupStep(step) {
-      this.step = step;
-    },
-  },
-};
+    ...mapActions('signup', [
+      'setSignupStep'
+    ]),
+  }
+}
 </script>
 
 <style>
